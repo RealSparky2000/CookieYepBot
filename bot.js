1a38c01899233afda120393322c1f2fbb7448427
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 17:51:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+if(message.content.startsWith(prefix))
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -19,3 +21,20 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+if(!message.content.startsWith(prefix)) return
+
+    if (message.content === prefix+'ping') {
+        message.channel.send('PONG!');
+      }
+    if (message.content === prefix+'bing') {
+        message.reply('BONG!');
+      }
+    if (message.content === prefix+'how are you') {
+        message.reply('NICE!');
+     }
+});
+
+
+client.login(process.env.BOT_TOKEN);
+
