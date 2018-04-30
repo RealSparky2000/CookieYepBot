59f090c10ee61d3a90e859ffd5fee78aa8fb9ef2
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 19:41:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,17 +49,5 @@ client.on('message', message => {
   	}
 });
 
- case "embed":
-       var embed = new Discord.RichEmbed()
-       .setAuthor('Заголовок с иконкой', client.user.avatarURL)
-       .setTitle('Второй заголовок без иконки')
-       .setDescription('Текст **внутри**')
-       .setImage(message.author.avatarURL)
-       .setColor(`0x${args[1]}`)
-       message.channel.send(embed)
-       break;
-       
- }})
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
