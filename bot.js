ad3b3ef8806ee3e5c0f13d4f2542510ef1f4fcee
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 23:06:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,53 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-var prefix = "&"
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
+var prefix = '&'
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
-
-client.on('message', message => {
-      if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'how-are-you') {
-    	message.reply('NICE!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'cookie') {
-    	message.reply('COOOOOOOOOKIES!!!!!!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'yep') {
-    	message.reply('Yep? Who is CookieYep? Hmmm...');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'invite') {
-    	message.reply('https://discordapp.com/oauth2/authorize?client_id=440527526746521601&scope=bot&permissions=8');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'yes') {
-    	message.reply('No, suck!');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
+    if(!message.content.startsWith(prefix)) return
+    if (message.content === prefix+'ping') {
+        message.channel.send('PONG!');
+      }
+    if (message.content === prefix+'bing') {
+        message.reply('BONG!');
+      }
+    if (message.content === prefix+'how are you') {
+        message.reply('NICE!');
+     }
+    if (message.content === prefix+'yep') {
+        message.reply('Yep? Who is CookieYep? Hm...');
+     }
+    if (message.content === prefix+'cookie') {
+        message.reply('COOOOOKIESS!!!!');
+     }
+    if (message.content === prefix+'yes') {
+        message.reply('No, suck!');
+     }
+    if (message.content === prefix+'no') {
+        message.reply('Yes..? 8(');
+     }
+});   
 client.login(process.env.BOT_TOKEN);
+ 
