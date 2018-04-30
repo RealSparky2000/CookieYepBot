96d565f3502727b299c7854b35c09fe51d468b6f
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 18:04:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+if(message.content.startsWith(prefix)) return
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
+var prefix = '&'
 
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
+    
+    //чтобы бот не реагировал если сообщение не начинается с префикса
+    if(!message.content.startsWith(prefix)) return
+
+    if (message.content === prefix+'ping') {
+        message.channel.send('PONG!');
+      }
+    if (message.content === prefix+'bing') {
+        message.reply('BONG!');
+      }
+    if (message.content === prefix+'how are you') {
+        message.reply('NICE!');
+     }
 });
 
-// THIS  MUST  BE  THIS  WAY
+
 client.login(process.env.BOT_TOKEN);
