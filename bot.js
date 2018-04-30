f54b9de3ff8260e545c2f4033887a14b3a0695c4
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 18:18:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,17 +7,34 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
+client.on('message', message => {    
+    //чтобы бот не реагировал если сообщение не начинается с префикса
+  if (!message.content.startsWith(PREFIX) || message.content.startsWith(PREFIX + PREFIX)) return
+   var args = message.content.substring(PREFIX.length).split(" ");
+  //метод
+       switch (args[0].toLowerCase()) {
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+       case "ping":
+       message.channel.send('PONG!');
+       break;
+
+       case "bing":
+      message.channel.send('BONG!');
+      break;
+
+   case "how-are-you":
+      message.channel.send('NICE!');
+       break;
+
+      case "embed":
+       var embed = new Discord.RichEmbed()
+       .setAuthor('Заголовок с иконкой', client.user.avatarURL)
+       .setTitle('Второй заголовок без иконки')
+       .setDescription('Текст **внутри**')
+       .setImage(message.author.avatarURL)
+       .setColor(`0x${args[1]}`)
+       message.channel.send(embed)
+       break;
+       
+ }})
