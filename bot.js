2acb61d48a9f8e66e7cdaa78683a811323363cce
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-04-30 18:48:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'how-are-you') {
+    	message.reply('NICE!');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
