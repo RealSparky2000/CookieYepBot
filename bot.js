f6fdf860a76993bc4f0010e51866fd78d6deb2ee
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-01 23:19:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,36 +66,5 @@ client.on('message', message => {
         }
      }
 
-const Discord = require('discord.js');
-
-const moment = require("moment");
-
-exports.run = async (client, message, args) => {
-    var user;
-	// If the user mentions someone, display their stats. If they just run userinfo without mentions, it will show their own stats.
-    if (message.mentions.users.first()) {
-      user = message.mentions.users.first();
-    } else {
-        user = message.author;
-    }
-    const member = message.guild.member(user);
-
-    const embed = new Discord.RichEmbed()
-		.setColor('RANDOM')
-		.setThumbnail(user.avatarURL)
-		.setTitle(`${user.username}#${user.discriminator}`)
-		.addField("ID:", `${user.id}`, true)
-		.addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
-		.addField("Created At:", `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-		.addField("Joined Server:", `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-		.addField("Bot:", `${user.bot}`, true)
-		.addField("Status:", `${user.presence.status}`, true)
-		.addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-		.addField("Roles:", member.roles.map(roles => `${roles.name}`).join(', '), true)
-		.setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
-     message.channel.send({embed});
-    }
-        var fetched = responses[Math.floor(Math.random() * responses.length)];
-ррпанр
 });
 client.login(process.env.BOT_TOKEN);
