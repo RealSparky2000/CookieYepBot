6323bedb77b576c04706bc6a785afef65994c8f5
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-02 14:46:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,92 +1,46 @@
 const Discord = require('discord.js');
-const moment = require("moment");
 const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
 var prefix = '&'
 client.on('message', message => {
-            var args = message.content.substring(prefix.length).split(" ");
-            if (!message.content.startsWith(prefix)) return
-            switch (args[0].toLowerCase()) {
-                case "ping":
-                    message.reply('PONG!');
-                    break;
-                case "bing":
-                    message.reply('BONG!');
-                    break;
-                case "how are you":
-                    message.reply('Nice!');
-                    break;
-                case "cookie":
-                    message.reply('COOOOOKIESS!!!!');
-                    break;
-                case "8ball":
-                    var responses = [
-                        'Agreed!',
-                        'Of Course!',
-                        'Nope.',
-                        'No',
-                        'Maybe',
-                        'One day...',
-                        'Don\'t see that happening!',
-                        'I have no idea'
-                    ]
-                    var fetched = responses[Math.floor(Math.random() * responses.length)];
-                    var embed = new Discord.RichEmbed()
-                        .setColor(0xffffff)
-                        .setFooter(fetched)
-                    message.channel.send({
-                        embed
-                    });
-                    break;
-                case "avatar":
-                    var embed = new Discord.RichEmbed()
-                        .setAuthor('CookieYep', client.user.avatarURL)
-                        .setTitle('The cookie steals avatars')
-                        .setDescription('[Avatar Link](' + message.author.avatarURL + ')')
-                        .setImage(message.author.avatarURL)
-                        .setColor("#ffcc00")
-                    message.channel.send({
-                        embed
-                    });
-                    break;
-                case "help":
-                    switch (args[1]) {
-                        case "avatar":
-                            message.channel.send('Displays your pfp.');
-                            break;
-                        default:
-                            message.channel.send('**All comands**: &cookie, &bing, &ping, &avatar, 8ball');
-                            break;
-                    }
-                case "info":
-                    var user;
-                    // If the user mentions someone, display their stats. If they just run userinfo without mentions, it will show their own stats.
-                    if (message.mentions.users.first()) {
-                        infouser = message.mentions.users.first();
-                    } else {
-                        infouser = message.author;
-                    }
-                    var infomember = message.guild.member(user);
-
-                    var infoembed = new Discord.RichEmbed()
-                        .setColor('RANDOM')
-                        .setThumbnail(user.avatarURL)
-                        .setTitle(`${infouser.username}#${infouser.discriminator}`)
-                        .addField("ID:", `${infouser.id}`, true)
-                        .addField("Nickname:", `${infomember.nickname !== null ? `${infomember.nickname}` : 'None'}`, true)
-            .addField("Bot:", `${infouser.bot}`, true)
-            .addField("Status:", `${infouser.presence.status}`, true)
-            .addField("Game:", `${infouser.presence.game ? infouser.presence.game.name : 'None'}`, true)
-            .addField("Roles:", infomember.roles.map(roles => `${roles.name}`).join(', '), true)
-            .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
-        message.channel.send({infoembed});
+    var args = message.content.substring(prefix.length).split(" ");
+    if(!message.content.startsWith(prefix)) return
+    switch(args[0]) {
+        case "ping":
+        message.reply('PONG!');
+        break;
+        case "bing":
+        message.reply('BONG!');
+        break;
+        case "how are you":
+        message.reply('Nice!');
+        break;
+        case "cookie":
+        message.reply('COOOOOKIESS!!!!');
+        break;
+        case "8ball":
+        var responses = [
+        'Agreed!',
+        'Of Course!',
+        'Nope.',
+        'No',
+        'Maybe',
+        'One day...',
+        'Don\'t see that happening!',
+        'I have no idea'
+        ]
+        var fetched = responses[Math.floor(Math.random() * responses.length)];
+        const embed = new Discord.RichEmbed()
+            .setColor(0xffffff)
+            .setFooter(fetched);
+        message.channel.send(embed);
         break;
         default:
         break;
     }
-    if (args[0].toLowerCase=="help") {
+    if (args[0]==="help") {
         if(args[1]==="avatar") {
             message.channel.send('Displays your pfp.');
         }
@@ -94,6 +48,33 @@ client.on('message', message => {
             message.channel.send('**All comands**: &cookie, &bing, &ping, &avatar, 8ball');
         }
      }
+    if (args[0]==='avatar') {
+        var embed = new Discord.RichEmbed()
+            .setAuthor('CookieYep', client.user.avatarURL)
+            .setTitle('The cookie steals avatars')
+            .setDescription('[Avatar Link](' + message.author.avatarURL + ')')
+            .setImage(message.author.avatarURL)
+            .setColor("#ffcc00")
+            message.channel.send({embed});
+    }
+case "8ball":
+var responses = [
+        'Agreed!',
+        'Of Course!',
+        'Nope.',
+        'No',
+        'Maybe',
+        'One day...',
+        'Don\'t see that happening!',
+        'I have no idea'
+    ]
+   
+    var fetched = responses[Math.floor(Math.random() * responses.length)];
 
+    const embed = new Discord.MessageEmbed()
+        .setColor(0xffffff)
+        .setFooter(fetched);
+    message.channel.send(embed);
+break;
 });
 client.login(process.env.BOT_TOKEN);
