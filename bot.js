062c881366aa6c46b75144b609d5b145a8af1c3f
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-02 17:32:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,6 @@ client.on('message', message => {
         }
         else {
             message.channel.send('**All comands**: &cookie, &bing, &ping, &avatar, 8ball');
-        break;
         }
      }
     
