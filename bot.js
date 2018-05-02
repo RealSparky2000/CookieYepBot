dce7188865b4484c7073a410fab173a0feccb3c3
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-02 16:45:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,9 +57,6 @@ client.on('message', message => {
             message.channel.send('**All comands**: &cookie, &bing, &ping, &avatar, 8ball');
         }
      }
-
-client.on("guildMemberAdd", function(member) {
-    let role = member.guild.roles.find("name", "Member");
-    member.addRole(role).catch(console.error);    
+    
 });
 client.login(process.env.BOT_TOKEN);
