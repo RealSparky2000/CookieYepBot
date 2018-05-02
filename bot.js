1019ae7af89f904d66d03f3a66607f334354c122
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-02 14:57:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
+bot.user.setGame('cookies');
 });
 var prefix = '&'
 client.on('message', message => {
