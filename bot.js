472f3202f119f974d8f21cbdd1b16ad97410006f
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-05-02 21:04:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on('message', message => {
             .setColor("#ffcc00")
             message.channel.send({embed});
         break;
-        case "infp":
+        case "info":
         let UserInfo = new Discord.MessageEmbed()
             .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
             .setTitle('UserInfo')
